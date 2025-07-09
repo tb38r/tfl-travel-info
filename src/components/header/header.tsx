e452475fcc62848743db5b0ec0242f94b1bcc9c5
Tolu <tlawal86@hotmail.co.uk>
2025-07-09 16:33:38 +0100

    autocomplete dropdown done

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,53 +1,8 @@
 import { Box, Typography } from "@mui/material";
 import Logo from "../../assets/tflLogo.svg";
-import { useEffect, useState } from "react";
-
-interface LineData{
-  stopPoints : any
-}
 
 export default function Header() {
 
-  const [lines, setLines] = useState<LineData |null>(null)
-  const [hasData, setHasData] = useState(false)
-  const [stops, setStops] = useState<Record<string, string>>({});
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch(
-          "https://api.tfl.gov.uk/StopPoint/Mode/tube"
-        );
-        if (!response.ok) {
-          console.error("Error fetching line data", response);
-          return;
-        }
-        const data = await response.json();
-        setLines(data);
-        setHasData(true)
-      } catch (err) {
-        console.error("Fetch failed:", err);
-      }
-    })();
-  }, []);
-
-  useEffect(()=>{
-
-    const newStops: Record<string, string> = {};
-
-    lines?.stopPoints.forEach((stop:any) => {
-      if (stop?.commonName && stop?.naptanId && stop?.naptanId.slice(0,3)=="940") {
-        newStops[stop.commonName] = stop.naptanId;
-      }
-    });
-    
-    setStops(newStops);
-    
-  }, [hasData])
-
-
-  console.log("map", lines)
-  console.log("stops", stops)
 
   return (
     <Box
@@ -74,7 +29,7 @@ export default function Header() {
     >
       <div
         className="logo"
-        style={{ height: "70px", width: "70px", zIndex: 1 }}
+        style={{ height: "90px", zIndex: 1 }}
       >
         <img
           src={Logo}
--- src/components/search-bar/searchStatus.tsx
+++ src/components/search-bar/searchStatus.tsx
@@ -1,32 +1,39 @@
-import { TextField, InputAdornment } from "@mui/material";
-import { Search } from "@mui/icons-material";
+import { TextField, Autocomplete } from "@mui/material";
+import { useMemo, useState } from "react";
+import { StopPoints } from "./types/stops";
 
 export default function SearchStatus() {
-  const handleStatusSearch = (e: React.ChangeEvent<any>) => {
-    console.log(e.target.value);
-  };
+  const [searchValue, setSearchValue] = useState("");
+
+  console.log("checkchange", searchValue);
+
+  const inputValues = useMemo(() => Object.keys(StopPoints), [StopPoints]);
+
   return (
-    
-    <TextField
-      sx={{
-        width: "99%",
-        "& .MuiOutlinedInput-root": {
-          borderRadius: "10px",
-          backgroundColor: '#E1EBEE',
-          height:'2.5rem'
-        },
+    <Autocomplete
+      disablePortal
+      options={inputValues}
+      autoComplete={true}
+      autoHighlight
+      onInputChange={(_, newValue: string) => {
+        setSearchValue(newValue);
       }}
-      slotProps={{
-        input: {
-          endAdornment: (
-            <InputAdornment position="end">
-              <Search />
-            </InputAdornment>
-          ),
-        },
-      }}
-      placeholder="Get Station Arrivals..."
-      onChange={handleStatusSearch}
+      // slotProps={{
+      //   listbox: { sx: { maxHeight: { s: "10px", lg: "50px" } } },
+      // }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          sx={{
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "10px",
+              backgroundColor: "#E1EBEE",
+              height: "2.5rem",
+            },
+          }}
+          placeholder="Get Station Arrivals"
+        />
+      )}
     />
   );
 }
